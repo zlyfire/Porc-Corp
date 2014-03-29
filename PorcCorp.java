7b0f02765409b190e848b993243d021caa132b81
zlyfire <zlyfire.martin@gmail.com>
2014-03-28 22:48:38 -0400

    Update PorcCorp.java

--- PorcCorp.java
+++ PorcCorp.java
@@ -1,23 +1,49 @@
 package com.zlyfire.porcorp;
 
+import java.util.logging.Logger;
+
 import net.minecraft.creativetab.CreativeTabs;
-import net.minecraft.init.Blocks;
-import net.minecraft.block.Block;
-import net.minecraft.init.Items;
 import net.minecraft.item.Item;
-import net.minecraft.item.ItemStack;
+
+import com.zlyfire.porcorp.utils.ConfigHandler;
+
+import cpw.mods.fml.common.FMLCommonHandler;
+import cpw.mods.fml.common.Loader;
 import cpw.mods.fml.common.Mod;
 import cpw.mods.fml.common.Mod.EventHandler;
 import cpw.mods.fml.common.event.FMLInitializationEvent;
 import cpw.mods.fml.common.event.FMLPostInitializationEvent;
 import cpw.mods.fml.common.event.FMLPreInitializationEvent;
 import cpw.mods.fml.common.registry.GameRegistry;
-import net.minecraftforge.oredict.*;
 
 @Mod(modid = PorcCorp.modid, version = PorcCorp.version)
 public class PorcCorp{
     public static final String modid = "porccorp";
     public static final String version = "0.0.1";
+	public static final Logger logger = Logger.getLogger("PorcCorp");
+    
+    public PorcCorp(){
+		logger.setParent(FMLCommonHandler.instance().getFMLLogger());
+		if(Loader.isModLoaded("IC2")){
+			PorcCorp.logger.info("[PorcCorp] Pummeling IC2 Into Submission");
+		}
+		if(Loader.isModLoaded("ThermalExpansion")){
+			PorcCorp.logger.info("[PorcCorp] Pummeling TE Into Submission");
+		}
+		if(Loader.isModLoaded("BuildCraft|Core")){
+			PorcCorp.logger.info("[PorcCorp] Pummeling BC Into Submission");
+		}
+		if(Loader.isModLoaded("AppliedEnergistics")){
+			PorcCorp.logger.info("[PorcCorp] Pummeling AE Into Submission");
+		}
+		if(Loader.isModLoaded("Factorization")){
+			PorcCorp.logger.info("[PorcCorp] Pummeling Factorization Into Submission");
+		}
+		if(Loader.isModLoaded("Forestry")){
+			PorcCorp.logger.info("[PorcCorp] Pummeling Forestry Into Submission");
+		}
+		PorcCorp.logger.info("[PorcCorp] Assimilating Universe");
+    }
     
 	public static CreativeTabs PorcTab = new CreativeTabs("Porc Corp"){
 		public Item getTabIconItem(){
@@ -27,6 +53,7 @@ public class PorcCorp{
     
     @EventHandler
     public void preinit(FMLPreInitializationEvent event){
+    	ConfigHandler.init(event.getSuggestedConfigurationFile());
     	registerBlocks();
     	registerItems();
     	PCCraft.registerRecipes();
@@ -43,7 +70,7 @@ public class PorcCorp{
     }
     
     public void registerBlocks(){    	
-    	PCItems.meatBlock = new MeatBlock().setBlockName("meatblock").setBlockTextureName(PorcCorp.modid + "meatblock" + 0);
+    	PCItems.meatBlock = new MeatBlock().setBlockName("meatblock")/*.setBlockTextureName(PorcCorp.modid + ":" + "meatblock" + 0)*/;
     	GameRegistry.registerBlock(PCItems.meatBlock, PCItems.meatBlock.getUnlocalizedName().substring(5));
     }
     public void registerItems(){
